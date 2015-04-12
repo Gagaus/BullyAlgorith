8034fa38513af3c284957532f67b5882ddb4b9b3
Flavio Silva <flavio.maxi@gmail.com>
2015-04-12 16:56:03 -0300

    Esqueleto1 - processo.c

--- proj2/processo.c
+++ proj2/processo.c
@@ -4,10 +4,17 @@
 #include <time.h>
 #include "ds.h"
 
+#define TIMEOUT 10
+#define SLEEP_TIME 1
+#define TIME_OF_DEATH 40
+
 int pid;    /* Process' id */
 char data[DATASIZE]; /* Process' data */
+int leader; // guarda o lider do ds
 
 typedef enum {ELECTION, OK, COORDINATOR, GENERIC} contentMsg;
+typedef enum {IDLE, DEAD, CALLED_ELECTION, WAITING_LEADER, LEADER} state;
+
 
 typedef struct msgbuf {
   long mtype;      /* message type */
@@ -31,26 +38,64 @@ void get_pid_from_argv(int argc, char* argv[]) {
 
 
 int main(int argc, char* argv[]) {
-  int l,i,j;
-  Msgbuf inbuf, outbuf;
-  int idLeader;
-  get_pid_from_argv(argc, argv);
-
-  get_queues();
-
-  srand(time(NULL));
-
-  while(true){
-	int e = rand() % 100;
-    switch (e) {
-    case 0: // morre
-		sleep(TIME_OF_DEATH);
-		// zerar queue
-		// chamar eleicao
-		break;
-	case 1: // manda msg pro lider
-		break;
+	int l,i,j;
+	state pState = IDLE;
+	Msgbuf inbuf, outbuf;
+	int idLeader;
+	get_pid_from_argv(argc, argv);
 	
-  }                 
-  return 0;
-}
+	get_queues();
+	
+	srand(time(NULL));
+	
+	while(true){
+		if (pState == IDLE){
+			int e = rand() % 100;
+			if (e == 1){ // manda msg pro lider
+				pState = WAITING_LEADER;
+			
+			}
+			else{
+				// espera um pouco
+				sleep (SLEEP_TIME)
+			}
+			
+		}
+		if (pState == LEADER){
+			int e = rand() % 100;
+			if (e==1) {
+				// MORRER.
+				pState = DEAD;
+			}
+			else{// RECEBER MENSAGENS GENERICAS. SE RECEBER, ENVIAR OUTRA DE VOLTA
+				
+			}
+		}
+		else if (pState == WAITING_LEADER){
+			sleep(TIMEOUT);
+			if (nowait_receive_message(pid, &inbuf, sizeof(Msgbuf)) < 0){
+				// LIDER MORREU!! :(
+				// pedir eleicao
+				
+			}
+		}
+		else if (pState == CALLED_ELECTION){
+			sleep(TIMEOUT);
+			if (nowait_receive_message(pid, &inbuf, sizeof(Msgbuf)) < 0){
+				// se nao ha mensagens pra mim, o lider sou eu! :D
+				// fazer broadcast 
+			}
+			else{
+				// se ha mensagem do tipo OK, minha parte acabou
+				pState = IDLE;
+			}
+		}
+		else if (pState == DEAD){
+			sleep(TIME_OF_DEATH);
+			// zerar minha fila
+			// pedir eleicao
+		}
+	}                 
+	return 0;
+	}
+	
